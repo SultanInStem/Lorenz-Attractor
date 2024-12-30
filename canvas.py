0428734851ba706f36e70eb954ccab273aeddc8e
= <sultaninstem004@gmail.com>
2024-12-30 00:03:44 -0800

    attractor class

--- canvas.py
+++ canvas.py
@@ -1,6 +1,6 @@
 import pygame
 import sys 
-
+from globals import to_math_coords, to_screen_coords
 
 class Canvas: 
     def __init__(self, size): 
@@ -15,6 +15,10 @@ class Canvas:
                 self.running = False
     def render(self):
         self.screen.fill((0,0,0))
+
+        pygame.draw.line(self.screen,(255,255,255),to_screen_coords((0,0), self.screen.get_size()), to_screen_coords((100,100), self.screen.get_size()))
+        # pygame.draw.circle(self.screen, (255,255,255), to_screen_coords((0,0), self.screen.get_size()),20,0)
+
         pygame.display.flip()
         self.clock.tick(60)
     def update(self): 
