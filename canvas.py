690a043cdfec98d1a40884a603c69bfd313a3d6f
= <sultaninstem004@gmail.com>
2024-12-29 20:17:28 -0800

    setup

--- canvas.py
+++ canvas.py
@@ -3,7 +3,22 @@ import sys
 
 
 class Canvas: 
-    def __init__(self): 
+    def __init__(self, size): 
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption("Lorenz")
+        self.clock = pygame.time.Clock()
+        self.running = True 
+    def handle_events(self): 
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                self.running = False
+
+    def run(self):
+        while(self.running):
+            pass 
+
+        
+        pygame.quit()
+        sys.exit()
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame 
+from canvas import Canvas 
 if __name__ == "__main__": 
     print("Hello bitches")
 
